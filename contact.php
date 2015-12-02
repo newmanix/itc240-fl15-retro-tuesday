c11dcbf91c038564c3fcf254d261ee79b4cf7fdc
Bill Newman <william.newman@seattlecolleges.edu>
2015-12-01 17:45:30 -0800

    Adds Contact database
    Uses prepared statements

--- contact.php
+++ contact.php
@@ -14,6 +14,44 @@
     $subject = 'Contact Form from retro site';
     
     safeEmail($to, $subject, $message);
+
+        //connect to the database in order to add contact data
+    $iConn = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die(myerror(__FILE__,__LINE__,mysqli_connect_error()));
+
+    //process each post var, adding slashes, using mysqli_real_escape(), etc.
+    $Name = dbIn($_POST['Name'],$iConn);
+    $Email = dbIn($_POST['Email'],$iConn);
+    $Comments = dbIn($_POST['Comments'],$iConn);
+
+    //place question marks in place of each item to be inserted
+    $sql = "INSERT INTO test_Contacts (Name,Email,Comments,DateAdded) VALUES(?,?,?,NOW())";
+    $stmt = @mysqli_prepare($iConn,$sql) or die(myerror(__FILE__,__LINE__,mysqli_error($iConn)));
+    
+    /*
+     * second parameter of the mysqli_stmt_bind_param below 
+     * identifies each data type inserted: 
+     *
+     * i == integer
+     * d == double (floating point)
+     * s == string
+     * b == blob (file/image)
+     *
+     *example: an integer, 2 strings, then a double would be: "issd"
+     */
+
+    mysqli_stmt_bind_param($stmt, 'sss',$Name,$Email,$Comments);
+
+    //execute sql command
+    mysqli_stmt_execute($stmt) or die();
+    
+    //close statement
+    @mysqli_stmt_close($stmt);
+    
+    //close connection
+    @mysqli_close($iConn);
+
+    
+    echo '<p>Thanks for the info!</p>';
     
     
 }else{//no data, show form
